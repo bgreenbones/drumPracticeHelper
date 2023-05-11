4dbdda147b52159f5a7807778bd55fc8a05def69
bgreenbones <gwoodben@gmail.com>
2023-05-11 19:45:06 -0400

    maybe have a rhythm display

--- lib/rhythmic_constraint.dart
+++ lib/rhythmic_constraint.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:rhythm_practice_helper/styles.dart";
 
 import "stickings.dart";
 
@@ -30,9 +31,19 @@ class RhythmicConstraintWidget extends StatefulWidget {
 class RhythmicConstraintWidgetState extends State<RhythmicConstraintWidget> {
   RhythmicConstraint constraint = RhythmicConstraint();
 
+  List<IconButton> getRhythmButtons() {
+    List<IconButton> result = [];
+    for (int i = 0; i < constraint.rhythm.length; i++) {
+      result.add(IconButton(
+          icon: Icon(Icons.widgets,
+              color: constraint.rhythm[i] ? trimColor : Colors.grey),
+          onPressed: () => {constraint.rhythm[i] = !constraint.rhythm[i]}));
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row();
-    // to do: display clickable square for each subdivision in rhythm length
+    return Row(children: getRhythmButtons());
   }
 }
